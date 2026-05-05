2df291f8ef9916d8e96a28dead9de4cd879d2fa1
Shubham kapoor <shubhamkapoor1807@gmail.com>
2026-05-06 02:38:52 +0530

    LinearSearch

---
+++ DiagonalMatrix.java
@@ -0,0 +1,47 @@
+class DiagonalMatrix
+ {
+ public static void main(String args[])
+ {
+ int[][] a = new int[][] { { 1, 0, 1},{ 0, 3, 0},{ 0, 0, 3} };
+ boolean setValue = true;
+ abc: for(int i = 0;i < a.length;i++)
+ {
+ for(int j = 0;j < a[i].length;j++)
+ {
+
+ if(i == j)
+ {
+ if(a[i][j] == 0)
+ {
+ setValue = false;
+ break abc;
+ }
+ }
+ else if(a[i][j] != 0)
+ {
+ setValue = false;
+ break abc;
+ }
+ }
+ }
+
+ System.out.println("The Given Matrix Value:");
+ for(int i = 0;i < a.length;i++)
+ {
+ for(int j = 0;j < a[i].length;j++)
+ {
+ System.out.print(a[i][j] + " ");
+ }
+ System.out.println();
+ }
+
+ if(setValue == true)
+ {
+ System.out.println("The Given Matrix is a Diagonal Matrix");
+ }
+ else
+ {
+ System.out.println("The Given Matrix is not a Diagonal Matrix");
+ }
+ }
+ }
