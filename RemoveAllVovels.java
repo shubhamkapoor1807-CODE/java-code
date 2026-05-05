ee221cfe2a29640dd65492eeaf091b928a3b5a64
Shubham kapoor <shubhamkapoor1807@gmail.com>
2026-05-06 02:52:24 +0530

    RemoveAllVovels

---
+++ RemoveAllVovels.java
@@ -0,0 +1,9 @@
+public class RemoveAllVovels {
+
+ public static void main(String[] args) {
+ String string = "Welcome to Candid Java Programming";
+ System.out.println("Input String : "+string);
+ string = string.replaceAll("[AaEeIiOoUu]", "");
+ System.out.println(string);
+ }
+ }
